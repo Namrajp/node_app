d13d12df2a845903278f020464f47bf982209fbc
Namraj Pudasaini <active99raj@gmail.com>
2022-07-02 12:44:04 +1000

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const express = require("express");
 const app = express();
 
-const { menu } = require("../user");
+
+const { menu } = require("./user");
+// Middleware
+
+app.use(bodyParser.json());
+app.use(cors());
 app.use(express.json());
 
-// app.use(express.static("./public"));
+app.use(express.static(path.resolve(__dirname,"./public")));
 // form submit user for authorize
 app.post("/contact", (req, res) => {
   const { name } = req.body;
@@ -13,8 +21,6 @@ app.post("/contact", (req, res) => {
 });
 
 app.get("/api/users", (req, res) => {
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.json(menu);
 });
 
@@ -26,6 +32,6 @@ app.get("/api/user/:id", (req, res) => {
   return res.status(200).json({ okey: true, product: product });
 });
 
-app.listen(5000, () => {
-  console.log("Server is listening on port 5000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is listening on port 3000");
 });
